bc105ae7b01d5647a8d9ce16b404cd2bfa8b69e0
prosrikant <lucky.srikant@gmail.com>
2024-03-17 23:16:02 +0530

    Initial commit of working model using ML.Net #WorkingModel

---
+++ ReviewPredict/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewPredict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide a Review here. The App will predict if its Positive or Negative : ");
+            var input = Console.ReadLine(); 
+            var output = PredictReview.Predict(new PredictReview.ModelInput() { Review = input });
+            Console.WriteLine($"Predicted Sentiment :-> {output.PredictedLabel}");
+            Console.ReadLine();
+        }
+    }
+}
